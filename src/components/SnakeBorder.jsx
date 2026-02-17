c2f3f0fbb0d2ab94654f5307a4a4a0204a6fb543
SaadAbdullah72 <saad489254@gmial.com>
2026-02-17 21:40:28 +0500

    style: revert Snake Border to previous CSS version

--- src/components/SnakeBorder.jsx
+++ src/components/SnakeBorder.jsx
@@ -4,21 +4,12 @@ import '../styles/SnakeBorder.css';
 const SnakeBorder = () => {
     return (
         <div className="snake-border-container">
-            <svg width="100%" height="100%" preserveAspectRatio="none">
-                <rect
-                    x="2"
-                    y="2"
-                    width="calc(100% - 4px)"
-                    height="calc(100% - 4px)"
-                    fill="none"
-                    stroke="var(--accent-color)"
-                    strokeWidth="4"
-                    className="snake-rect"
-                />
-            </svg>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
     );
 };
-
 
 export default SnakeBorder;
